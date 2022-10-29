1e6b26e5fd755c085e62d89fdc5c2c3d056beb1f
roald944 <hypotenous8777@gmail.com>
2022-10-29 16:50:18 +0800

    database query
    database query

---
+++ database_query/authentication_db.sql
@@ -0,0 +1,34 @@
+/*
+
+ Source Server         : mysql
+ Source Server Type    : MySQL
+ Source Server Version : 100422
+ Source Host           : localhost:3306
+ Source Schema         : authentication_db
+
+ Target Server Type    : MySQL
+ Target Server Version : 100422
+ File Encoding         : 65001
+
+ Date: 29/10/2022 16:47:03
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `token` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `is_active` enum('0','1') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `date_time` date NOT NULL,
+  `time_stamp` timestamp NOT NULL DEFAULT current_timestamp ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`, `username`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
